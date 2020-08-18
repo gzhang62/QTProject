9c3dc53d8bece844728f5f78bb82246390252d35
zhang <zhang_hanzhou@hotmail.com>
2020-08-18 14:29:43 -0400

    changed vertexes, fixed off

---
+++ offreader.cpp
@@ -0,0 +1,6 @@
+#include "offreader.h"
+
+OffReader::OffReader()
+{
+
+}
---
+++ offreader.h
@@ -0,0 +1,11 @@
+#ifndef OFFREADER_H
+#define OFFREADER_H
+
+
+class OffReader
+{
+public:
+    OffReader();
+};
+
+#endif // OFFREADER_H
